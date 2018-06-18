0663fcadad274839dc4a3c2e4051a591e9622d79
shIne_wOng <1551885@tongji.edu.cn>
2018-06-18 15:59:53 +0800

    令人期待已久的最终版程序(希望吧
    在昨天排序的基础上
    1.增加了去除冗余。其中去除冗余写了三个版本的函数，因为前两个运行太慢emm.....，但是第三个版本感觉是强行做的，所以可读性可能不强
    2.增加了线性插值，这里放弃了自己的尊严在python中用了for循环......，数据缺失的点往往会缺失很多个，而不是只有一个，所以这个插值的结果可能误差还挺大的

--- traffic_readData.py
+++ traffic_readData.py
@@ -101,9 +101,10 @@ def detectorTiming(detectorValues):
     # the same principle as 'groupId', however, this dict is used to separate the 
     # traffic data by date
     dateId = {'18':data_18,'19':data_19,'20':data_20,'21':data_21,
-              '22':data_22,'23':data_23,'24':data_24}
+              '22':data_22,'23':data_23,'24':data_24
+              }
     
-    # 'lineOfData' is a 'trafficInfo' struture, which is define above
+    # 'lineOfData' is a 'trafficInfo' struture, which is defined above
     processLine = lambda lineOfData:dateId.get(str(lineOfData['date'].day)).append(lineOfData)
     
     list(map(processLine,detectorValues))
@@ -111,28 +112,122 @@ def detectorTiming(detectorValues):
 
 '''
 @brief   : To remove the duplicate datas
-@variable: the whole series of data read by a single dector, e.g.'NHNX39_1'
+@variable: the whole series of data read by a single dector, e.g.'NHNX39_1', _ordered_
 @return  : None
-@others  : 
-
-def removeRedundancy(lineOfData):
+@others  : this function should be called on the basis of sorting
 '''
-    
-list(map(readSheet,allSheets))
+# This version of removeRedundancy would operate around 3 minutes for each list like NHNX39_1.......
+def removeRedundancy(detectorValues):
+    for element in detectorValues[:-1]:# remove the last element        
+        # locate the real-time index of the current element
+        index = detectorValues.index(element)
+        print(index)
+        # prevent index overflow due to removing some elements
+        if index == len(detectorValues)-1:
+            break
+        nextElement = detectorValues[index+1]
+        
+        currentTime = element['date']
+        nextTime    = nextElement['date']
+        if currentTime == nextTime:
+            detectorValues.pop(index)
 
-# sort all the datas stored in those user-defined lists in the order of time
+# Well, this version using 'map' instead of 'for' loop operate two times quicker than the first one. 
+# Still, it would take one minute and a half before it finishes processing.
+def removeRedundance(detectorValues):
+    def processLine(element):
+        index =  detectorValues.index(element)  
+        print(index)
+        # prevent index overflow due to removing some elements
+        if index >= len(detectorValues)-1:
+            return
+        nextElement = detectorValues[index+1]
+        
+        currentTime = element['date']
+        nextTime    = nextElement['date']
+        if currentTime == nextTime:
+            detectorValues.pop(index)
+    
+    list(map(processLine,detectorValues))
+
+# The ultimate version of 'removeRedundancy', it operates fast and the result is convincing.
+# However, in order to settle various problems and bugs, the programme structure is somewhat unreadable
+def removeRedund(detectorValues):
+    listLength = len(detectorValues)
+    for index in range(1,listLength-1):
+        # prevent index overflow
+        if index >= len(detectorValues):
+            break
+        
+        prevTime    = detectorValues[index-1]['date']
+        currentTime = detectorValues[index]['date']
+        if index < len(detectorValues)-1:
+            nextTime    = detectorValues[index+1]['date']
+            
+            if currentTime == nextTime:
+                detectorValues.pop(index) 
+                
+        if currentTime == prevTime:
+             detectorValues.pop(index)
+    
+'''
+@brief   : To fix the missing data using lineal interpolation
+@variable: the whole series of data read by a single dector, e.g.'NHNX39_1', _ordered_
+@return  : None
+@others  : this function should be called on the basis of sorting
+'''
+DATANUM = 4320*7
+def linearInterp(detectorValues):
+    for index in range(DATANUM-1):
+        currData = detectorValues[index]
+        nextData = detectorValues[index+1]
+        currTimestamp = currData['date'].timestamp()
+        nextTimestamp = nextData['date'].timestamp()
+        
+        if currTimestamp == nextTimestamp - 20:
+            continue
+        
+        missingNum = (nextTimestamp-currTimestamp-20)/20
+        # construct a new trafficInfo for inserting       
+        newDate   = datetime.fromtimestamp(currTimestamp+20)
+        newVolume = (nextData['volume']-currData['volume'])/(missingNum+1)+currData['volume']
+        newSpeed  = (nextData['speed'] -currData['speed']) /(missingNum+1)+currData['speed']
+        newOccupy = (nextData['occupy']-currData['occupy'])/(missingNum+1)+currData['occupy']
+        trafficInfo = {'volume':newVolume, 'speed':newSpeed,
+                       'occupy':newOccupy, 'date' : newDate}
+        
+        # insert constrcuted trafficInfo
+        detectorValues.insert(index+1,trafficInfo)
+
+list(map(readSheet,allSheets))
+# remove those useless datas
+groupId.pop('NHWX41(3)      ')
+groupId.pop('NHWX41(2)      ')
+groupId.pop('NHWX41(1)      ')
+
+
+# sort all the datas stored in those user-defined lists in the order of time, for loop version
+'''
 for detectors in groupId.values():
     detectors.sort(key = lambda lineOfData:lineOfData['date'].timestamp())
+'''
+# another solution without using for loop
+list(map(lambda detector:detector.sort(key = lambda lineOfData:lineOfData['date'].timestamp()),groupId.values()))
 
+# remove the duplicate datas in these lists
+list(map(removeRedund,groupId.values()))
 
+#fix the missing data using lineal interpolation
+list(map(linearInterp,groupId.values()))
 # distribute the datas into an array of each day, that is to say, now e.g. NHNX39_1 is an list
 # of seven lists of the trafficInfo on one single day
+
 NHNX39_1    = detectorTiming(NHNX39_1)
 NHNX39_2    = detectorTiming(NHNX39_2) 
 NHNX40_1    = detectorTiming(NHNX40_1)
 NHNX40_2    = detectorTiming(NHNX40_2)
 NHNX41_1    = detectorTiming(NHNX41_1)
-NHNX41_2    = detectorTiming(NHNX41_2)
+NHNX41_2    = detectorTiming(NHNX41_2)()
 NHWN_NI_1_1 = detectorTiming(NHWN_NI_1_1)
 NHWN_NI_1_2 = detectorTiming(NHWN_NI_1_2)
 NHWN_NI_2_1 = detectorTiming(NHWN_NI_2_1)
